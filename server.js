1bef8c7330031a0e7f76f3d013e7236e041847f3
Pedro Belo <pedro@heroku.com>
2015-02-18 14:09:45 -0800

    basic check for router logs

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ app.set('port', (process.env.PORT || 5000));
 app.use(logfmt.bodyParser());
 
 app.post('/logs', function(req, res) {
-  console.log('received log:')
-  console.log(req.body)
+  req.body.forEach(function(log) {
+    if (log.heroku && log.router  && log.dyno && log.status) {
+      var dyno = log.dyno.replace(/\w+\./, "")
+      console.log("dyno " + dyno + " status=" + log.status)
+    }
+  })
   res.status(200).end()
 });
 
