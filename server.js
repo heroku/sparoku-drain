5f817552f23b5c7d13a5078b82e10298e9bea7c6
Pedro Belo <pedro@heroku.com>
2015-02-18 20:14:36 -0800

    organize

--- server.js
+++ server.js
@@ -3,22 +3,25 @@ var logfmt  = require('logfmt');
 var spark   = require('spark')
 var sparoku = require('./lib/sparoku.js')
 
+// get the spark device
 var device = null;
 spark.login({ accessToken: process.env.SPARK_TOKEN });
 spark.getDevice(process.env.SPARK_DEVICE_ID, function(err, d) {
   device = d;
 })
 
-var app = express();
-app.set('port', (process.env.PORT || 5000));
-app.use(logfmt.bodyParser());
-
+// eek, store requests in a global for now. this can go to redis later
 var requests = []
 
-// every second: update device with dyno summaries
+// update the device with dyno summaries once a second
 setInterval(function() {
   sparoku.update(requests, device)
 }, 1000)
+
+// setup Express app to handle the logs
+var app = express();
+app.set('port', (process.env.PORT || 5000));
+app.use(logfmt.bodyParser());
 
 app.post('/logs', function(req, res) {
   req.body.forEach(function(log) {
