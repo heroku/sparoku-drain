b93fdddeddef74e1c555dadfcf4c0a60d7b760c5
Pedro Belo <pedro@heroku.com>
2015-02-18 19:03:41 -0800

    using same constants

--- server.js
+++ server.js
@@ -3,11 +3,16 @@ var app = express();
 var logfmt = require('logfmt');
 var spark = require('spark')
 
+var device = null;
 spark.login({ accessToken: process.env.SPARK_TOKEN });
-spark.getDevice(process.env.SPARK_DEVICE_ID, function(err, device) {
-  device.callFunction('update', '50')
+spark.getDevice(process.env.SPARK_DEVICE_ID, function(err, d) {
+  device = d;
 })
 
+const OFF = 0;
+const GREEN = 1;
+const YELLOW = 2;
+const RED = 3;
 
 app.set('port', (process.env.PORT || 5000));
 app.use(logfmt.bodyParser());
@@ -33,21 +38,23 @@ setInterval(function() {
 
   var update = Object.keys(status).map(function(dyno) {
     var diff = status[dyno].success - status[dyno].error
-    var color = (diff>0)? 'green' : 'red'
+    var color = (diff>0)? GREEN : RED
     return dyno + '=' + color
   })
 
   var rawUpdate = update.reduce(function(prev, update) {
     return prev + ';' + update
-  })
+  }) + ';'
 
   console.log(rawUpdate)
 
 }, 1000)
 
-setInterval(function() {
-  requests.push({ dyno: 1, status: 200 })
-}, 100)
+// to test:
+// setInterval(function() {
+//   requests.push({ dyno: 1, status: 200 })
+//   requests.push({ dyno: 2, status: 500 })
+// }, 100)
 
 app.post('/logs', function(req, res) {
   req.body.forEach(function(log) {
