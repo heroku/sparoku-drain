56bab0fed74140ddc713bae1c39c32b151182848
Pedro Belo <pedro@heroku.com>
2015-02-18 21:17:50 -0800

    more tests, reuse colors

--- test/sparoku_test.js
+++ test/sparoku_test.js
@@ -3,8 +3,33 @@ var sparoku = require('../lib/sparoku.js');
 
 describe('Sparoku', function() {
   describe('getColor', function() {
-    it('is OFF when dyno does not exist', function() {
-      assert.equal(0, sparoku.getColor(42, {}))
+    it('is off when dyno does not exist', function() {
+      assert.equal(sparoku.colors.off, sparoku.getColor(42, {}))
+    });
+
+    it('is blue when dyno is idle', function() {
+      sparoku.dynos = { 1: 'idle' }
+      assert.equal(sparoku.colors.blue, sparoku.getColor(1, {}))
+    });
+
+    it('is blue when dyno is starting', function() {
+      sparoku.dynos = { 1: 'starting' }
+      assert.equal(sparoku.colors.blue, sparoku.getColor(1, {}))
+    });
+
+    it('is red when dyno crashed', function() {
+      sparoku.dynos = { 1: 'crashed' }
+      assert.equal(sparoku.colors.red, sparoku.getColor(1, {}))
+    });
+
+    it('is red when dyno is serving 500s', function() {
+      sparoku.dynos = { 1: 'up' }
+      assert.equal(sparoku.colors.red, sparoku.getColor(1, { success: 1, error: 2 }))
+    });
+
+    it('is green when dyno is working normally', function() {
+      sparoku.dynos = { 1: 'up' }
+      assert.equal(sparoku.colors.green, sparoku.getColor(1, { success: 2, error: 1 }))
     });
   });
 })
