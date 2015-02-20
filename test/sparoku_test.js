ea9be4602ebbc9f6852761073538a05cc2d10fe0
Pedro Belo <pedro@heroku.com>
2015-02-19 17:45:59 -0800

    handle the serving state
    it's different than up because the button will render a flashing
    green light

--- test/sparoku_test.js
+++ test/sparoku_test.js
@@ -4,19 +4,23 @@ var sparoku = require('../lib/sparoku.js');
 describe('Sparoku', function() {
   describe('getState', function() {
     it('assumes down when state is not set', function() {
-      assert.equal(sparoku.states.down, sparoku.getState(null, {}))
+      assert.equal(sparoku.states.down, sparoku.getState(null))
     });
 
     it('handles dynos idle', function() {
-      assert.equal(sparoku.states.idle, sparoku.getState('idle', {}))
+      assert.equal(sparoku.states.idle, sparoku.getState('idle'))
     });
 
     it('handles dynos starting', function() {
-      assert.equal(sparoku.states.booting, sparoku.getState('starting', {}))
+      assert.equal(sparoku.states.booting, sparoku.getState('starting'))
     });
 
     it('handles crashed dynos', function() {
-      assert.equal(sparoku.states.crashed, sparoku.getState('crashed', {}))
+      assert.equal(sparoku.states.crashed, sparoku.getState('crashed'))
+    });
+
+    it('handles dynos that are just up', function() {
+      assert.equal(sparoku.states.up, sparoku.getState('up'))
     });
 
     it('flags dynos rendering a lot of 500s', function() {
@@ -24,9 +28,9 @@ describe('Sparoku', function() {
       assert.equal(sparoku.states.error, sparoku.getState('up', stats))
     });
 
-    it('indicates dynos working normally', function() {
+    it('flags dynos serving requests', function() {
       var stats = { success: 2, error: 1 }
-      assert.equal(sparoku.states.up, sparoku.getState('up', stats))
+      assert.equal(sparoku.states.serving, sparoku.getState('up', stats))
     });
   });
 
